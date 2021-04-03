66c81629ad0ba7675e5dd115785bdafd796be014
Gleb <watch_over10@mail.ru>
2021-04-04 00:41:58 +0300

    SecondEnd

---
+++ cards.php
@@ -0,0 +1,15 @@
+<div class="cards-block">
+<h2>Специальные Предложения</h2>    
+<form class="cards" method="POST" action="">
+    <button class="card">
+        <img src="IMG/Coldrex.png" alt="File not found" class="card-image">
+        <p class="card-title">Колдрекс ХотРем</p>
+        <p class="card-discription">Порошок для приготовления раствора для приема внутрь
+</p>
+        <p class="card-price">От 300 р</p>
+        <form method="POST" action="">
+            <input type="button" class="card-btn" value='В корзину'>
+        </form>
+    </button>
+</form>
+</div>
--- sections.php
+++ sections.php
@@ -1,38 +1,87 @@
-<form class="sections">
+<form class="sections" method="POST" action="" id="ProductTypeForm">
+    <?php
+
+    require_once 'connection.php';
     
-    <button class="line">
-        <img src="IMG/nose.png" alt="Nose" class="section-line-pick">
-        <p class="section-line-text">От простуды</p>
-        <div class="arrow"></div>
+    $link = mysqli_connect($host, $user, $password, $database)
+    or die("Error" . mysqli_error($link));
+
+    $result = $link->query('SELECT * FROM producttype');
+    $productTypeName = $result->fetch_all(MYSQLI_ASSOC);
+    ?>
+    
+    <button class="line" id="line">
+    <img src="ICONS/nose.svg" alt="Img not found" class="section-line-pick">
+    <p class="section-line-text" id="ProductType-name">От простуды</p>
+    <div class="arrow"></div>
     </button>
     <hr class="line-divider">
 
-    <button class="line">
-        <img src="IMG/nose.png" alt="Nose" class="section-line-pick">
-        <p class="section-line-text">От простуды</p>
-        <div class="arrow"></div>
+    <button class="line" id="line">
+    <img src="ICONS/Vitamins.svg" alt="Img not found" class="section-line-pick">
+    <p class="section-line-text" id="ProductType-name">Витамины</p>
+    <div class="arrow"></div>
     </button>
     <hr class="line-divider">
 
-    <button class="line">
-        <img src="IMG/nose.png" alt="Nose" class="section-line-pick">
-        <p class="section-line-text">От простуды</p>
-        <div class="arrow"></div>
+    <button class="line" id="line">
+    <img src="ICONS/cosmetic.svg" alt="Img not found" class="section-line-pick">
+    <p class="section-line-text" id="ProductType-name">Косметика</p>
+    <div class="arrow"></div>
     </button>
     <hr class="line-divider">
 
-    <button class="line">
-        <img src="IMG/nose.png" alt="Nose" class="section-line-pick">
-        <p class="section-line-text">От простуды</p>
-        <div class="arrow"></div>
+    <button class="line" id="line">
+    <img src="ICONS/Eye.svg" alt="Img not found" class="section-line-pick">
+    <p class="section-line-text" id="ProductType-name">Зрение</p>
+    <div class="arrow"></div>
     </button>
     <hr class="line-divider">
 
-    <button class="line">
-        <img src="IMG/nose.png" alt="Nose" class="section-line-pick">
-        <p class="section-line-text">От простуды</p>
-        <div class="arrow"></div>
+    <button class="line" id="line">
+    <img src="ICONS/Child.svg" alt="Img not found" class="section-line-pick">
+    <p class="section-line-text" id="ProductType-name">Для детей</p>
+    <div class="arrow"></div>
     </button>
     <hr class="line-divider">
 
-</form
+    <button class="line" id="line">
+    <img src="ICONS/diabet.svg" alt="Img not found" class="section-line-pick">
+    <p class="section-line-text" id="ProductType-name">Диабет</p>
+    <div class="arrow"></div>
+    </button>
+    <hr class="line-divider">
+
+    <button class="line" id="line">
+    <img src="ICONS/tests.svg" alt="Img not found" class="section-line-pick">
+    <p class="section-line-text" id="ProductType-name">Тесты</p>
+    <div class="arrow"></div>
+    </button>
+    <hr class="line-divider">
+
+</form>
+
+<script>
+
+// let productTypeName = <?= json_encode($productTypeName)?>;
+
+// console.log(productTypeName);
+
+// function loadProductType(){
+//     let line = '<button class="line" id="line"><img src="" id="ProductType-image" alt="Во время загрузки изображения возникла ошибка" class="section-line-pick"> <p class="section-line-text" id="ProductType-name"></p> <div class="arrow"></div> </button><hr class="line-divider">'
+    
+//     productTypeName.forEach(obj => {
+//         line = (line.value == obj.ProductTypeId ? line + `<p class="section-line-text" id="ProductType-name">'${obj.ProductTypeName}'</p> <img src='${obj.ProductTypeImage}' id="ProductType-image" alt="Во время загрузки изображения возникла ошибка" class="section-line-pick"> <p class="section-line-text" id="ProductType-name">'${obj.ProductTypeName}'</p> <div class="arrow"></div>` : line );
+//     })
+//     ProductTypeForm.innerHTML = line;
+// }
+
+// function addAllProductType(){
+//     let line = '<button class="line" id="line"><img src="" id="ProductType-image" alt="Во время загрузки изображения возникла ошибка" class="section-line-pick"> <p class="section-line-text" id="ProductType-name"></p> <div class="arrow"></div> </button><hr class="line-divider">'
+//     productTypeName.forEach(obj => line +=`<p class="section-line-text" id="ProductType-name">'${obj.ProductTypeName}'</p> <img src='${obj.ProductTypeImage}' id="ProductType-image" alt="Во время загрузки изображения возникла ошибка" class="section-line-pick"> <p class="section-line-text" id="ProductType-name">'${obj.ProductTypeName}'</p> <div class="arrow"></div>`);
+//     ProductTypeForm.innerHTML = line;
+// }
+
+// addAllProductType()
+// loadProductType()
+</script>
